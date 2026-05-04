98fee05a1c06e00a9e23bbc057dc3e3d168ffb94
dealexis <flytzitv@gmail.com>
2026-05-04 23:15:10 +0200

    feat: add login and registration pages with GitHub login option

---
+++ app/(auth)/register/page.tsx
@@ -0,0 +1,83 @@
+import { GitHubIcon } from "@/components/icons/GitHub";
+import { Logo } from "@/components/icons/logo";
+import {
+  InputGroup,
+  InputGroupInput,
+  InputGroupLabel,
+} from "@/components/ui/MyInput";
+
+export default function RegisterPage() {
+  return (
+    <div className="flex min-h-screen items-center justify-center p-6 md:p-10">
+      <div className=" max-w-lg w-full flex flex-col items-center gap-6 ">
+        <div className="w-full flex flex-col items-center justify-center gap-1.5">
+          <Logo size={60} />
+          <h1 className="text-xl font-bold">Welcome to Projex</h1>
+          <p className="text-sm leading-normal font-normal text-muted-foreground ">
+            Already have an account?{" "}
+            <a
+              href="/login"
+              className="underline hover:text-primary transition-colors duration-500"
+            >
+              Log in
+            </a>
+          </p>
+        </div>
+
+        <form className="w-full flex flex-col gap-6">
+          <InputGroup>
+            <InputGroupLabel htmlFor="name">Name</InputGroupLabel>
+            <InputGroupInput id="name" placeholder="Enter your name" />
+          </InputGroup>
+
+          <InputGroup>
+            <InputGroupLabel htmlFor="email">Email</InputGroupLabel>
+            <InputGroupInput
+              id="email"
+              type="email"
+              placeholder="Enter your email"
+            />
+          </InputGroup>
+
+          <InputGroup>
+            <InputGroupLabel htmlFor="password">Password</InputGroupLabel>
+            <InputGroupInput
+              id="password"
+              type="password"
+              placeholder="Enter your password"
+            />
+          </InputGroup>
+
+          <div className="flex flex-col gap-3">
+            <button
+              type="submit"
+              className="w-full bg-foreground text-background h-9 text-sm flex flex-row items-center justify-center gap-2 font-medium rounded-md cursor-pointer hover:bg-foreground/70 transition-all duration-500 active:scale-98 "
+            >
+              Create Account
+            </button>
+
+            <Separator />
+
+            <button
+              type="submit"
+              className="w-full border border-border bg-muted/20 h-9 text-sm flex flex-row items-center justify-center gap-2 font-medium rounded-md cursor-pointer transition-all duration-500 hover:bg-muted/50 hover:text-foreground active:scale-98 "
+            >
+              <GitHubIcon size={16} />
+              Continue with GitHub
+            </button>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+}
+
+function Separator() {
+  return (
+    <div className="flex items-center gap-3 w-full">
+      <div className="h-px bg-muted flex-1" />
+      <span className="text-sm text-muted-foreground">Or continue with</span>
+      <div className="h-px bg-muted flex-1" />
+    </div>
+  );
+}
---
+++ components/ui/MyInput.tsx
@@ -0,0 +1,41 @@
+import { cn } from "../../lib/utils";
+
+function InputGroup({ children }: { children: React.ReactNode }) {
+  return (
+    <div className={cn("w-full flex flex-col gap-2 items-start")}>
+      {children}
+    </div>
+  );
+}
+
+function InputGroupLabel({
+  className,
+  ...props
+}: React.ComponentProps<"label">) {
+  return (
+    <label
+      className={cn(
+        "flex items-center gap-2 text-sm leading-none font-medium select-none group-data-[disabled=true]:pointer-events-none group-data-[disabled=true]:opacity-50 peer-disabled:cursor-not-allowed peer-disabled:opacity-50",
+        className,
+      )}
+      {...props}
+    />
+  );
+}
+
+function InputGroupInput({
+  className,
+  ...props
+}: React.ComponentProps<"input">) {
+  return (
+    <input
+      className={cn(
+        "h-9 w-full min-w-0 rounded-md border border-input bg-transparent px-2.5 py-1 text-base transition-colors outline-none file:inline-flex file:h-6 file:border-0 file:bg-transparent file:text-sm file:font-medium file:text-foreground placeholder:text-muted-foreground focus-visible:border-ring focus-visible:ring-3 focus-visible:ring-ring/50 disabled:pointer-events-none disabled:cursor-not-allowed disabled:bg-input/50 disabled:opacity-50 aria-invalid:border-destructive aria-invalid:ring-3 aria-invalid:ring-destructive/20 md:text-sm dark:bg-input/30 dark:disabled:bg-input/80 dark:aria-invalid:border-destructive/50 dark:aria-invalid:ring-destructive/40",
+        className,
+      )}
+      {...props}
+    />
+  );
+}
+
+export { InputGroup, InputGroupLabel, InputGroupInput };
